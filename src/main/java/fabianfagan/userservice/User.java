d9c137f9260f814941f96ef246abea70316cede8
Fabian <fabian.fagan99@gmail.com>
2021-07-20 14:17:21 +1200

    first commit

---
+++ src/main/java/fabianfagan/userservice/User.java
@@ -0,0 +1,42 @@
+package fabianfagan.userservice;
+
+public class User {
+    private String email; 
+    private String password; 
+    private String firstName; 
+    private String lastName; 
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " email='" + getEmail() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            "}";
+    }
+   
+}
---
+++ src/test/java/fabianfagan/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fabianfagan.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
